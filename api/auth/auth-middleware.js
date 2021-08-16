76b335b29b0ba6cc5374da9721ab891ab8854e83
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-16 14:55:42 -0600

    setting auth-middleware up

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -8,14 +8,7 @@ function restricted(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body already exists in the database
 
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
 async function checkUsernameFree(req, res, next) {
   try{
     const exists = await User.findBy(req.body.username)
@@ -31,31 +24,27 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
+async function checkUsernameExists(req, res, next) {
+  try{
+    const users = await User.findBy({ username: req.body.username })
+    if(!users.length) {
+      req.user = users[0]
+      next()
+    } else {
+      next({ status: 401, message: 'Invalid credentials'})
+    }
   }
-*/
-function checkUsernameExists() {
-
+  catch(err){
+    next(err)
+  }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+function checkPasswordLength(req, res, next) {
+  if(!req.body.password || req.body.password < 3) {
+    next({ status: 422, message: 'Password must be longer than 3 chars'})
   }
-*/
-function checkPasswordLength() {
-
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   restricted,
   checkUsernameFree,
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,38 +3,14 @@ const router = require('express').Router()
 const User = require('./users-model');
 const { restricted } = require('../auth/auth-middleware');
 
-router.get('/', restricted, (req, res, next) => {
+router.get('/', restricted, (req, res, next) => {//eslint-disable-line
   User.find()
     .then(users => {
       res.status(200).json(users)
     })
-    .catch(err => {
+    .catch(err => { //eslint-disable-line
       res.status(401).json({ message: 'You shall not pass!'})
     })
 })
-/**
-  [GET] /api/users
 
-  This endpoint is RESTRICTED: only authenticated clients
-  should have access.
-
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    },
-    // etc
-  ]
-
-  response on non-authenticated:
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
- */
-
-
-// Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router;
