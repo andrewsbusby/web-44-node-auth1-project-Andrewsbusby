f16bd9f1bdb470bfdc50598c584b7bbd98fa0e9d
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-16 16:47:08 -0600

    cleaning up model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -2,17 +2,17 @@
 const db = require('../../data/db-config');
 
 function find() {
-  return db('users').select('user_id', 'username')
+  return db('users')
 }
 
 
 function findBy(filter) {
-  return db('users').where('filter', filter)
+  return db('users').where(filter)
 }
 
 
 function findById(user_id) {
-  return db('users').select('user_id', 'username').where('user_id', user_id).first()
+  return db('users').where('user_id', user_id).first()
 }
 
 
